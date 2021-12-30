19d0ee4abd0b6d21fe259280941b929f85d05c7e
Ahmed Ashraf Ahmed <89888246+ahmeddashraff@users.noreply.github.com>
2021-12-31 01:11:14 +0200

    ay haga

--- src/main/java/com/example/demo/Admin.java
+++ src/main/java/com/example/demo/Admin.java
@@ -1,23 +1,31 @@
 package com.example.demo;
 
+import com.example.demo.application.IPeopleService;
+import com.example.demo.application.PeopleService;
 import com.example.demo.observers.Driver;
 
 public class Admin {
 
-    private UserManager usermanager;
+    private IPeopleService peopleService = new PeopleService();
 
-    public void verifyDriver() {
-        if (usermanager.getUser() instanceof Driver) {
-            if (((Driver) usermanager.getUser()).isPending()) {
-                ((Driver) usermanager.getUser()).setPending(false);
-                System.out.println("admin has accepted your registeration");
-            }
-        }
+
+    public void listDriverPendingRegisteration()
+    {
+        
     }
 
-    public void listDriverPendingRegisteration() {
-        for (int i = 0; i < usermanager.getPendingReg().size(); i++) {
-            System.out.println(usermanager.getPendingReg().get(i).getName());
-        }
-    }
+    // public void verifyDriver() {
+    //     if (peopleService.getUser() instanceof Driver) {
+    //         if (((Driver) peopleService.getUser()).isPending()) {
+    //             ((Driver) peopleService.getUser()).setPending(false);
+    //             System.out.println("admin has accepted your registeration");
+    //         }
+    //     }
+    // }
+
+    // public void listDriverPendingRegisteration() {
+    //     for (int i = 0; i < peopleService.getPendingReg().size(); i++) {
+    //         System.out.println(peopleService.getPendingReg().get(i).getName());
+    //     }
+    // }
 }
--- src/main/java/com/example/demo/application/PeopleService.java
+++ src/main/java/com/example/demo/application/PeopleService.java
@@ -39,4 +39,42 @@ public class PeopleService implements IPeopleService {
     {
         return persistence.login(u);
     }
+
+    // private User user;
+
+    // public ArrayList<User> getPendingReg() {
+    //     return pendingReg;
+    // }
+
+    // private ArrayList<User> pendingReg = new ArrayList<User>();
+
+    // public void setPendingReg(User user) {
+    //     pendingReg.add(user);
+    // }
+
+    // public void setUser(User user) {
+    //     this.user = user;
+    // }
+
+    // public User getUser() {
+    //     return user;
+    // }
+
+    // // Persistance persistance = new UserSavings();
+
+    // public void register() {
+    //     if (user instanceof Driver) {
+    //         if (((Driver) user).isPending() == false) {
+    //             // persistance.create(user);
+    //         } else {
+    //             // do nothing
+    //         }
+    //     } else {
+    //         // persistance.create(user);
+    //     }
+    // }
+
+    // public void login() {
+    //     // persistance.read(user);
+    // }
 }
